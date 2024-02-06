51f488cc3f9dc3d21c199a75fe434ff9995e5bf2
JamesIsWack <JamesIsWack@github.com>
2024-02-06 17:55:57 -0600

    Testing command line args

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "./include/g_functions.hpp"
+#define VERSION "v0.1-dev"
+int main(int argc, char* argv[]) {
+    scanf("%s", &argv[1]);
+    if (argv[1] == "--version" || argv[1] == "-v") {
+        printf("3d Game Engine %s\nLicensed under GNU GPLv3", VERSION);
+        return 0;
+    }
 
-int main() {
     NxWindow::Spawn();
     return 0;
 }

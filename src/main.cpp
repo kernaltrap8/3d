28d44946cf00e92e4ac6febf40a588b7d05f1ede
JamesIsWack <JamesIsWack@github.com>
2024-02-06 18:17:17 -0600

    Too many arguments

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,10 @@
 #include "./include/g_functions.hpp"
 #define VERSION "v0.1-dev"
 int main(int argc, char* argv[]) {
-    // check if argv[] has multiple arguments
+    // check if argc[] has multiple arguments
     if (argc >= 2) {
         printf("Too many arguments.\n");
+        return 1;
     }
     if (argv[1] == std::string_view("--version") || argv[1] == std::string_view("-v")) {
         printf("3d Game Engine %s\nLicensed under GNU GPLv3\n", VERSION);

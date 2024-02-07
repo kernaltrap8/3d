cf6dee1189f83740625c11bb04274f7c9eae93bc
JamesIsWack <JamesIsWack@github.com>
2024-02-06 18:14:06 -0600

    Still working on command line args...

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string_view>
 #include "./include/g_functions.hpp"
 #define VERSION "v0.1-dev"
 int main(int argc, char* argv[]) {
-    scanf("%s", &argv[1]);
-    if (argv[1] == "--version" || argv[1] == "-v") {
-        printf("3d Game Engine %s\nLicensed under GNU GPLv3", VERSION);
+    // check if argv[] has multiple arguments
+    if (argc >= 2) {
+        printf("Too many arguments.\n");
+    }
+    if (argv[1] == std::string_view("--version") || argv[1] == std::string_view("-v")) {
+        printf("3d Game Engine %s\nLicensed under GNU GPLv3\n", VERSION);
         return 0;
     }
 

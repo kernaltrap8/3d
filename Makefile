10af1abb7909bd17f1b8c336d190fda7e3ffb547
JamesIsWack <JamesIsWack@github.com>
2024-02-06 20:22:39 -0600

    More optimized

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-make:
-	g++ ./src/*.cpp -o ./3d -lX11
+3d: main.o 3d.o window.o
+	g++ build/main.o build/3d.o build/window.o -o 3d -lX11
+main.o:
+	mkdir -p build
+	g++ -c src/main.cpp -o build/main.o
+3d.o:
+	g++ -c src/3d.cpp -o build/3d.o
+window.o:
+	g++ -c src/window.cpp -o build/window.o
+clean:
+	rm *.o
--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,14 @@
 int main(int argc, char* argv[]) {
     // check if argc[] has multiple arguments
     if (argc >= 2) {
-        printf("Too many arguments.\n");
-        return 1;
-    }
-    if (argv[1] == std::string_view("--version") || argv[1] == std::string_view("-v")) {
-        printf("3d Game Engine %s\nLicensed under GNU GPLv3\n", VERSION);
-        return 0;
-    }
-
+        if (argv[1] == std::string_view("--version") || argv[1] == std::string_view("-v")) {
+            printf("3d Game Engine %s\nLicensed under GNU GPLv3\n", VERSION);
+            return 0;
+        } else {
+            printf("Usage:\n-v or --version: Display current version of 3d being run.\n");
+            return EXIT_FAILURE;
+        }
+    } 
     NxWindow::Spawn();
     return 0;
 }
